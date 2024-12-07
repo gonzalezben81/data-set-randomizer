fb5c64f9e338d2f23d75ffd56a74236be8b9ef4e
gonzalezben81 <gonzalezben393@gmail.com>
2024-12-07 09:38:14 +0000

    Updated and added dataset randomizer.

---
+++ randomized_data_set.R
@@ -0,0 +1,20 @@
+# Set the seed for reproducibility
+set.seed(123)
+
+# Load the necessary library
+library(dplyr)
+library(readxl)
+
+original_data_set<- readxl::read_excel(path = "./HumanResourcesDataAA5221_Reduced_Fall2.xlsx",sheet = 1)
+
+# Randomize specified columns in the dataset
+modified_data_set <- original_data_set %>%
+  mutate(
+    satisfaction_level = satisfaction_level + 0.03,
+    last_evaluation = last_evaluation - 0.02,
+    left = sample(c(0, 1), size = n(), replace = TRUE, prob = c(0.2, 0.8)),
+    Work_accident = sample(c(0, 1), size = n(), replace = TRUE, prob = c(0.7, 0.3))
+  )
+
+# Optionally, save the modified dataset
+write.csv(modified_data_set, "modified_data_set.csv", row.names = FALSE)
